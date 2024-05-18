753207547d307ae87ac5e93e214e634538df0687
vishanksingh <vishanksinghh@gmail.com>
2024-05-18 16:57:34 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import Logo from '../assets/fcc logo high res.png'
 
 function Navbar() {
   return (
-    <nav className="fixed w-full h-[10vh] flex justify-between px-[2vw]">
-      <div className=" h-full">
-        <img src={Logo} alt="FCC Logo" className="h-full" />
+    <nav className="fixed w-full h-[12vh] flex justify-between px-[4vw]">
+      <div className=" h-full grid items-center">
+        <img src={Logo} alt="FCC Logo" className="h-[3.5vw]" />
       </div>
-      <ul>
+      <ul className="flex justify-between items-center gap-[2vw] text-[1vw] font-poppins font-light">
         <li><a href="/">Home</a></li>
         <li><a href="/horizon">Horizon</a></li>
         <li><a href="/schedule">Schedule</a></li>
