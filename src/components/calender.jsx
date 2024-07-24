db5d47c8db289084e9d247dcdbb45b7913dbc47e
Manne Rithvik <139128024+rithvik-2006@users.noreply.github.com>
2024-07-24 20:33:48 +0530

    Create calender.jsx

---
+++ src/components/calender.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import axios from "axios";
+
+const ReactCalendar = () => {
+  const [date, setDate] = useState(new Date());
+  const [events, setEvents] = useState([]);
+
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+  };
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/events')
+      .then((response) => {
+        setEvents(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const showEvents = () => {
+    return events
+      .filter(event => new Date(event.date).toDateString() === date.toDateString())
+      .map((event, index) => <p key={index}>{event.title}</p>);
+  };
+
+  return (
+    <div>
+      <Calendar
+        className="react-calendar"
+        onChange={handleDateChange}
+        value={date}
+      />
+      <p className="date">{date.toDateString()}</p>
+      {showEvents()}
+    </div>
+  );
+};
+
+export default ReactCalendar;
