99cefeac118547164507f3c3dd3afda376810002
Mahesh <mahesh.ruppa.1234@gmail.com>
2024-07-30 13:29:50 +0530

    Update YearTile.jsx

--- src/components/YearTile.jsx
+++ src/components/YearTile.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-function YearTile({year}) {
+function YearTile({year, setYearChange }) {
+
+  const changeYear = (e) => {
+    const data = e.target.value;
+    setYearChange(data);
+  };
+  
   return (
     <div>
       <button className='bg-custom-bg2 text-white
-        w-[8vw] h-[6vh] rounded-3xl flex justify-center items-center font-medium text-2xl'>
+        w-[8vw] h-[6vh] rounded-3xl flex justify-center items-center font-medium text-2xl'
+        onClick={changeYear} value={year}>
         {year}
       </button>
     </div>
