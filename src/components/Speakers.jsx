3293f13668edcf0d985d5b42ca2098f56fc01e45
Mahesh <mahesh.ruppa.1234@gmail.com>
2024-07-30 13:29:14 +0530

    Update Speakers.jsx

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import psimg from '../assets/images/pastspeakers/s1.png'
 import YearTile from './YearTile';
 
 function Speakers() {
-    const ps_images = [
+    const ps_images_2022 = [
         { img_id: psimg }, { img_id: psimg },{ img_id: psimg },{ img_id: psimg },
     ];
+    const ps_images_2023 = [
+      { img_id: psimg }, { img_id: psimg },{ img_id: psimg },
+    ];
+    const ps_images_2024 = [
+      { img_id: psimg }, { img_id: psimg },{ img_id: psimg },{ img_id: psimg },{ img_id: psimg },
+    ];
+    
+    const [year, setYear] = useState(localStorage.getItem("year") || "2024");
+
+    const handleYearChange = (selectedYear) => {
+      setYear(selectedYear);
+      localStorage.setItem("year", selectedYear);
+    };
+    
+
   return (
     <div>
         <div className='flex flex-col h-auto w-full items-center justify-center my-6'>
@@ -15,17 +30,40 @@ function Speakers() {
         </div>
         <div className='px-5 my-6 flex flex-row w-full'>
 					<div className='w-[13vw] h-[45vh] mr-8 flex flex-col items-center justify-evenly'>
-						<YearTile year={2022}/>
-						<YearTile year={2023}/>
-						<YearTile year={2024}/>
+						<YearTile year= "2022" setYearChange={handleYearChange}/>
+						<YearTile year= "2023" setYearChange={handleYearChange}/>
+						<YearTile year= "2024" setYearChange={handleYearChange}/>
 						<YearTile />
 						<YearTile />
 					</div>
-          <div className='flex justify-evenly flex-wrap md-1:gap-3'>
-            {ps_images.map((image,index)=>(
-              <img className='w-[25%] h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
-            ))}
-          </div>
+          {year === "2022" && (
+            <>
+              <div className='flex justify-evenly flex-wrap md-1:gap-3'>
+                {ps_images_2022.map((image,index)=>(
+                  <img className='w-[25%] h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
+                ))}
+              </div>             
+            </>
+          )}
+          {year === "2023" && (
+            <>
+              <div className='flex justify-evenly flex-wrap md-1:gap-3'>
+                {ps_images_2023.map((image,index)=>(
+                  <img className='w-[25%] h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
+                ))}
+              </div>             
+            </>
+          )}
+          {year === "2024" && (
+            <>
+              <div className='flex justify-evenly flex-wrap md-1:gap-3'>
+                {ps_images_2024.map((image,index)=>(
+                  <img className='w-[25%] h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
+                ))}
+              </div>             
+            </>
+          )}
+
         </div>
       </div>
     </div>
