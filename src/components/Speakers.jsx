442d54d459a212828ab4632bf6d76eedb1eb1145
Anand <ep22btech11012@iith.ac.in>
2024-07-30 15:52:12 +0530

    Update Speakers.jsx

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -29,7 +29,7 @@ function Speakers() {
           <p className='font-rosario mt-0 mb-4 text-7xl md-1:text-5xl md-1:w-full md-1:text-center'>Speakers</p>
         </div>
         <div className='px-5 my-6 flex flex-row w-full'>
-					<div className='w-[13vw] h-[45vh] mr-8 flex flex-col items-center justify-evenly'>
+					<div className='w-1/4 h-full mr-8 flex flex-col items-center justify-between gap-3'>
 						<YearTile year= "2022" setYearChange={handleYearChange}/>
 						<YearTile year= "2023" setYearChange={handleYearChange}/>
 						<YearTile year= "2024" setYearChange={handleYearChange}/>
@@ -37,31 +37,31 @@ function Speakers() {
 						<YearTile />
 					</div>
           {year === "2022" && (
-            <>
+            <div className='w-3/4'>
               <div className='flex justify-evenly flex-wrap md-1:gap-3'>
                 {ps_images_2022.map((image,index)=>(
-                  <img className='w-[25%] h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
+                  <img className='h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
                 ))}
               </div>             
-            </>
+            </div>
           )}
           {year === "2023" && (
-            <>
+            <div className='w-3/4'>
               <div className='flex justify-evenly flex-wrap md-1:gap-3'>
                 {ps_images_2023.map((image,index)=>(
-                  <img className='w-[25%] h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
+                  <img className=' h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
                 ))}
               </div>             
-            </>
+            </div>
           )}
           {year === "2024" && (
-            <>
+            <div className='w-3/4'>
               <div className='flex justify-evenly flex-wrap md-1:gap-3'>
                 {ps_images_2024.map((image,index)=>(
-                  <img className='w-[25%] h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
+                  <img className=' h-auto p-2 flex-wrap md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
                 ))}
               </div>             
-            </>
+            </div>
           )}
 
         </div>
