65961b075328ad4f35b0480bc10140a62ac019f4
vishanksingh <vishanksinghh@gmail.com>
2024-07-31 20:58:13 +0530

    most changes done

--- src/pages/Horizon.jsx
+++ src/pages/Horizon.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
+import Speakers from '../components/Speakers'
 
-import image1 from "../assets/horizon-about/1.svg"
-import image2 from "../assets/horizon-about/2.svg"
-import image3 from "../assets/horizon-about/3.svg"
-import image4 from "../assets/horizon-about/4.svg"
+import image1 from '../assets/horizon-about/p1.png'
+import image2 from '../assets/horizon-about/p2.png'
+import image3 from '../assets/horizon-about/p3.png'
+import image4 from '../assets/horizon-about/p4.png'
 
-import psimg1 from "../assets/pastspeakers/akhilsharma.svg"
-import psimg2 from "../assets/pastspeakers/gauravmahindra.svg"
-import psimg3 from "../assets/pastspeakers/rishabhjain.svg"
-import psimg4 from "../assets/pastspeakers/subbaperepa.svg"
 
-import simg1 from "../assets/sponsors/abhibus-min.png"
-import simg2 from "../assets/sponsors/decathalon-min.png"
-import simg3 from "../assets/sponsors/easemytrip-min.png"
-import simg4 from "../assets/sponsors/edufabrica-min.png"
-import simg5 from "../assets/sponsors/elearnmarkets-min.png"
-import simg6 from "../assets/sponsors/finlarics-min.png"
-import simg7 from "../assets/sponsors/grabon-min.png"
-import simg8 from "../assets/sponsors/monster-min.png"
-import simg9 from "../assets/sponsors/plum-min.png"
-import simg10 from "../assets/sponsors/preplounge-min.png"
-import simg11 from "../assets/sponsors/product-min.png"
-import simg12 from "../assets/sponsors/sid farm-min.png"
-import simg13 from "../assets/sponsors/startupnews-min.png"
-import simg14 from "../assets/sponsors/stockgro-min.png"
+import psimg1 from '../assets/pastspeakers/akhilsharma.svg'
+import psimg2 from '../assets/pastspeakers/gauravmahindra.svg'
+import psimg3 from '../assets/pastspeakers/rishabhjain.svg'
+import psimg4 from '../assets/pastspeakers/subbaperepa.svg'
+
+
+import simg1 from '../assets/sponsors/abhibus-min.png'
+import simg2 from '../assets/sponsors/decathalon-min.png'
+import simg3 from '../assets/sponsors/easemytrip-min.png'
+import simg4 from '../assets/sponsors/edufabrica-min.png'
+import simg5 from '../assets/sponsors/elearnmarkets-min.png'
+import simg6 from '../assets/sponsors/finlarics-min.png'
+import simg7 from '../assets/sponsors/grabon-min.png'
+import simg8 from '../assets/sponsors/monster-min.png'
+import simg9 from '../assets/sponsors/plum-min.png'
+import simg10 from '../assets/sponsors/preplounge-min.png'
+import simg11 from '../assets/sponsors/product-min.png'
+import simg12 from '../assets/sponsors/sid farm-min.png'
+import simg13 from '../assets/sponsors/startupnews-min.png'
+import simg14 from '../assets/sponsors/stockgro-min.png'
 
 
 function Horizon() {
@@ -32,9 +35,7 @@ function Horizon() {
     { img_id: image1 }, { img_id: image2 }, { img_id: image3 }, { img_id: image4 }
   ];
 
-  const ps_images = [
-    { img_id: psimg1 }, { img_id: psimg2 }, { img_id: psimg3 }, { img_id: psimg4 }
-  ];
+
 
   const s_images = [
     { img_id: simg1 }, { img_id: simg2 }, { img_id: simg3 }, { img_id: simg4 }, { img_id: simg5 },
@@ -44,60 +45,63 @@ function Horizon() {
 
   return (
     <div className='text-white font-roboto'>
-
       {/* Landing part Horizon */}
       <div className='h-auto px-6 py-12 my-12 text-center  '>
-        <p className='text-vw-2.3 font-normal text-dark-blue pb-6 md-1:pb-4 md-1:text-xl'>
+        <p className='text-vw-2.3 font-light font-rosario text-white pb-6 md-1:pb-4 md-1:text-xl'>
           FCC IIT-HYDERABAD Presents
         </p>
         <div className='mx-auto w-full flex-col gap-0 m-6 md-1:mb-6 mt-0'>
-          <p className='w-full font-normal text-vw-10 md:text-vw-10 md-1:text-6xl md-1:leading-normal'>HORIZON 24'</p>
+          <p className='w-full font-normal text-vw-10 text-custom-blue md:text-vw-10 md-1:text-6xl md-1:leading-normal'>HORIZON 25'</p>
         </div>
-        <div className="gap-2 flex justify-center py-8 md-1:py-2">
-          <button className="bg-custom-butt-bg hover:bg-custom-butt-bg-hover rounded-lg text-xl px-6 py-2 md-1:w-70p md-1:inline-block
-          font-normal ">TODAY'S SCHEDULE </button>
-        </div>
+
+        {/* Schedule Button */}
+        {/* <div className="gap-2 flex justify-center py-8 md-1:py-2">
+          <button className="font-rosario bg-custom-blue font-bold rounded-lg text-xl px-6 py-2 md-1:w-70p md-1:inline-block">
+            TODAY'S SCHEDULE </button>
+        </div> */}
+
       </div>
 
+
       {/* About the Event */}
-      <div className="mt-10p ml-auto mr-auto w-full h-auto flex flex-col items-center p-1p 
-        justify-center bg-custom-grad-1">
+      <div className="mt-10p ml-auto mr-auto w-full h-auto flex flex-col items-center p-1p
+        justify-center bg-gradient-to-r from-custom-gradient-2 to-custom-gradient-1">
         <div className='m-auto w-90p my-4'>
-          <div className="md-1:hidden w-full flex items-baseline h-50p text-vw-7 pt-2p pl-2p text-left leading-100p
+          <div className="font-rosario md-1:hidden w-full flex items-baseline h-50p text-vw-7 pt-2p pl-2p text-left leading-100p
             font-medium">
             About,
             <br></br>
             the Event
           </div>
-          <div className='md-2:hidden flex justify-center w-full items-baseline h-50p text-vw-7 pt-2p pl-2p pb-4'>
+          <div className='font-rosario md-2:hidden flex justify-center w-full items-baseline h-50p text-vw-7 pt-2p pl-2p pb-4'>
             About the Event
           </div>
-          <div className="w-full h-auto text-custom-white text-vw-2.75 font-extralight leading-normal p-2p 
-            md-1:text-vw-5 md-1:text-center">Horizon 2024, the flagship event of FCC IITH, brings in some of the best speakers and leaders to promote the knowledge of Finance, Consulting,
-            and Product Management among the community via thought-provoking Keynote Talks, Panel Discussions,
-            Career Guidance Meetups, Networking Lobbies, Workshops and Competitions.
+          <div className="w-full h-auto text-custom-white text-vw-2.75 font-extralight leading-normal p-2p
+            md-1:text-vw-5 md-1:text-center">Horizon 2025, the flagship event of FCC IITH, brings in some of the best speakers and leaders to promote the knowledge of Finance, Consulting,
+                                             and Product Management among the community via thought-provoking Keynote Talks, Panel Discussions,
+                                             Career Guidance Meetups, Networking Lobbies, Workshops and Competitions.
           </div>
 
           <div className='w-full h-auto flex justify-evenly items-center flex-wrap my-4'>
-            <div className='w-full flex flex-wrap justify-center'>
+            <div className='w-full flex flex-wrap justify-between'>
               {images.map((image, index) => (
                 <img className='md-1:w-1/2 md-2:w-23p' key={index} src={image.img_id} alt={`Image ${index + 1}`} />
               ))}
             </div>
           </div>
 
-          <div className='md-1:hidden flex items-baseline h-1/2 leading-100p text-left font-medium text-vw-7 pt-2p pl-2p'>
+          <div className='font-rosario md-1:hidden flex items-baseline h-1/2 leading-100p text-left font-medium text-vw-7 pt-2p pl-2p'>
             Our Vision
             <br></br>
-            for 2024
+            for 2025
           </div>
-          <div className='md-2:hidden flex items-baseline h-1/2 leading-100p justify-center font-medium text-vw-7 pt-2p pl-2p md-1:pb-4'>
-            Our Vision for 2024
+          <div className='font-rosario md-2:hidden flex items-baseline h-1/2 leading-100p justify-center font-medium text-vw-7 pt-2p pl-2p md-1:pb-4'>
+            Our Vision for 2025
           </div>
 
           <div className='w-full h-auto text-custom-white text-vw-2.75 font-extralight leading-normal p-2p
               md-1:text-vw-5 md-1:text-center'>
-            In 2023-24,  Horizon aims to expand its influence to students community by inviting industry, academia, and government experts to share insights on Finance, Consulting,
+            In 2024-25,  Horizon aims to expand its influence to students community by inviting industry, academia, and government experts to share insights on Finance, Consulting,
             and Product Management.
             <p className='mt-0 mb-4'>We will introduce new competitions and workshops solidifying its status as the go-to platform for Finance, Consulting, and Product Management enthusiasts.</p>
           </div>
@@ -105,23 +109,13 @@ function Horizon() {
       </div>
 
       {/* Past Speakers */}
-      <div className='flex flex-col h-auto w-full items-center justify-center my-6'>
-        <div className='flex items-center justify-center w-90p my-6'>
-          <p className='mt-0 mb-4 text-7xl md-1:text-5xl md-1:w-full md-1:text-center'>Past Speakers</p>
-        </div>
-        <div className='px-12 my-6 flex justify-content-evenly w-full'>
-          <div className='flex justify-evenly flex-wrap md-1:gap-3'>
-            {ps_images.map((image,index)=>(
-              <img className='w-23p h-auto md-1:w-2/5 md-1:flex-wrap ' src= {image.img_id} key={index} alt={`Image ${index + 1}`}></img>
-            ))}
-          </div>
-        </div>
-      </div>
+
+      <Speakers/>
 
       {/* Sponsers */}
       <div className='flex flex-col justify-center items-center overflow-hidden relative my-6'>
         <div className='flex items-center justify-center'>
-          <p className='text-center mb-4 text-7xl md-1:text-5xl'>Sponsors</p>
+          <p className='font-rosario text-center mb-5 text-7xl md-1:text-5xl'>Sponsors</p>
         </div>
         <div className='flex w-full gap-3p animate-scroll-1'>
           <div className='flex basis-1/5 md-1:flex md-1:basis-1/4 gap-3p'>
@@ -135,4 +129,5 @@ function Horizon() {
   )
 }
 
-export default Horizon  
+export default Horizon
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,17 @@ export default {
       colors:{
         'dark-blue' :'#1270ed',
         'custom-white':'#f2f2f2',
+        'custom-blue' :'rgba(18, 112, 237, 1)',
+        'custom-gradient-1' : 'rgba(18, 109, 237, 0.9)',
+        'custom-gradient-2' : 'rgba(18, 112, 237, 0.6)',
       },
       backgroundColor:{
-        'custom-bg' : 'rgba(0, 0, 0, 1)',
+        'custom-bg' : 'rgba(30, 30, 30, 1)',
         'custom-butt-bg':'#0d6efd',
         'custom-butt-bg-hover':'#0b5ed7',
+        'custom-blur-bg':'rgba(126, 71, 241, 0.4)',
+        'custom-blue' :'rgba(18, 112, 237, 1)',
+        'custom-bg2' : '#0C4393',
       },
       backgroundImage:{
         'custom-grad-1':'linear-gradient(to bottom,rgba(15, 70, 147, 0.9),rgba(15, 72, 147, 0.6))',
@@ -44,11 +50,18 @@ export default {
         '2p' : '2%',
         '3p' : '3%',
         '10p': '10%',
+        '20p': '20%',
         '23p': '23%',
         '45p': '45%',
         '50p': '50%',
         '70p': '70%',
         '90p': '90%',
+        'vw-15' : '15vw',
+        'vw-20' : '20vw',
+        'vw-30' : '30vw',
+        'vw-60' : '60vw',
+        'vh-50' : '50vh',
+        'vh-60' : '60vh',
       },
       lineHeight: {
         normal: 'normal',
@@ -64,7 +77,8 @@ export default {
         }
       },
       screens:{
-        'sm' : { 'max': '420px' },
+        'sm'   : {'max': '420px'},
+        'md-3' : {'max': '1000px'},
         'md-1' : {'max': '800px'},
         'md-2' : {'min': '800px'},
       },
