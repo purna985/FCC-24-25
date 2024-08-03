2bd12d9bbdc38408fc14afc7a7803a20f51793a8
vishanksingh <vishanksinghh@gmail.com>
2024-08-03 17:05:17 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from './pages/Home'
-import Horizon from "./pages/Horizon.jsx";
-import Schedule from "./pages/Schedule.jsx";
-import Sponsors from "./pages/Sponsors.jsx";
-import Team from "./pages/Team.jsx";
-import Resources from "./pages/Resources.jsx";
+import Horizon from "./pages/Horizon";
+import Schedule from "./pages/Schedule";
+import Sponsors from "./pages/Sponsors";
+import Team from "./pages/Team";
+import Resources from "./pages/Resources";
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
