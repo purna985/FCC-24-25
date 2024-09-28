b706bd73cc89a204c9c72593f7223b50ed9562b7
Anand <ep22btech11012@iith.ac.in>
2024-09-28 16:10:45 +0530

    Update Speakers.jsx

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -43,13 +43,13 @@ function Speakers() {
     <div>
       <div className='flex flex-col w-full items-center justify-center my-6'>
         <div className='flex items-center justify-center w-90p my-6'>
-          <p className='font-rosario mt-0 mb-4 text-7xl md-1:text-5xl md-1:w-full md-1:text-center'>Speakers</p>
+          <p className='font-rosario mt-0 mb-4 text-7xl md-1:text-5xl md-1:w-full md-1:text-center'>Previous Speakers</p>
         </div>
         <div className='px-5 my-6 flex flex-row w-full'>
           <div className='w-1/4 h-full mr-8 flex flex-col items-center justify-between gap-3'>
-            <YearTile year= "2022" setYearChange={handleYearChange}/>
-            <YearTile year= "2023" setYearChange={handleYearChange}/>
             <YearTile year= "2024" setYearChange={handleYearChange}/>
+            <YearTile year= "2023" setYearChange={handleYearChange}/>
+            {/* <YearTile year= "2022" setYearChange={handleYearChange}/> */}
             {/* <YearTile />
 						<YearTile /> */}
           </div>
