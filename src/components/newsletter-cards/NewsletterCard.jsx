c9358dd21ad324905e8f2dc30e47c2db2f1e237a
Anand <ep22btech11012@iith.ac.in>
2024-11-03 14:27:55 +0530

    newsletter template changes

--- src/components/newsletter-cards/NewsletterCard.jsx
+++ src/components/newsletter-cards/NewsletterCard.jsx
@@ -1,8 +1,14 @@
 import { NewsLetterContentData } from '../../assets/data/newsletters/1'
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 const renderContentWithBreaks = (text) => {
     return text.split('\n').map((line, index) => (
-        <p className='my-2' key={index}>{line}</p>
+        <p className='my-2 text-md' key={index}>{line}</p>
     ));
 };
 
@@ -13,39 +19,45 @@ const NewsletterCard = ({ id }) => {
     const images = data.images
     const authors = data.authors
     return (
-        <div className='min-h-screen ' >
+        <div className='min-h-screen my-10 ' >
+            
             <div className='w-full flex' >
+                <div className='w-[90%] mx-auto md:w-[80%] p-4' >
+                    <h1 className='text-4xl my-4 font-bold text-blue-400 ' >FCC NEWSLETTER</h1>
+                    <h1 className='text-3xl mt-4 font-bold ' >{data.title}</h1>
+                    <h1 className='text-xl mb-4 text-gray-500 ' >{data.date}</h1>
+                    <Accordion type="single" collapsible className="w-full">
+                        {
+                            data.headings.map((data, key) => {
+                                return (
 
-                <div className='w-[20%]' >
+                                    <AccordionItem value={`item-${key}`} className='mb-10' key={key}>
+                                        <AccordionTrigger>
+                                            <h2 className='text-xl my-2 font-semibold'>{data}</h2>
 
-                </div>
-                <div className='w-[80%] p-4' >
-                    <h1 className='text-3xl my-4 font-bold ' >{data.title}</h1>
-                    {
-                        data.headings.map((data, key) => {
-                            return (
-                                <div className='mb-10' key={key}>
-                                    <h2 className='text-xl my-2 font-semibold'>{data}</h2>
-                                    <div className='flex flex-col w-[80%] ' >
-                                        {renderContentWithBreaks(content[key])}
-                                        \
-                                        <div>
-                                            {
-                                                images[key] && <img src={images[key]} alt=""  className='w-64 h-64' />
-                                            }
-                                        </div>
-                                    </div>
-                                    <hr className='my-3' />
-                                </div>
-                            );
+                                        </AccordionTrigger>
+                                        <AccordionContent className='flex flex-col w-[80%] ' >
+                                            {renderContentWithBreaks(content[key])}
+                                            \
+                                            <div>
+                                                {
+                                                    images[key] && <img src={images[key]} alt="" className='w-64 h-64' />
+                                                }
+                                            </div>
+                                        </AccordionContent>
 
-                        })
-                    }
+                                    </AccordionItem>
+                                );
+
+                            })
+                        }
+                    </Accordion>
 
                     <div>
                         <h1 className='text-xl my-2 font-semibold' >Written by {authors[0]} and {authors[1]} </h1>
                     </div>
                 </div>
+
             </div>
         </div>
 
