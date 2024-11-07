a10c26b6e837ed46dda15a4a655543cc35c01092
Anand <126659841+anandk3012@users.noreply.github.com>
2024-11-07 18:30:54 +0530

    updated newsletters routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,57 +12,42 @@ import ProdMan from "./pages/resources-pages/ProdMan";
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import News_1 from "./pages/newsletters/newsletters/News_1";
-import News_2 from "./pages/newsletters/newsletters/News_2";
-import News_3 from "./pages/newsletters/newsletters/News_3";
-import News_4 from "./pages/newsletters/newsletters/News_4";
-import News_5 from "./pages/newsletters/newsletters/News_5";
-import News_6 from "./pages/newsletters/newsletters/News_6";
-import News_7 from "./pages/newsletters/newsletters/News_7";
-import News_8 from "./pages/newsletters/newsletters/News_8";
-import News_9 from "./pages/newsletters/newsletters/News_9";
-import News_10 from "./pages/newsletters/newsletters/News_10";
+import NewsletterCard from "./components/newsletter-cards/NewsletterCard";
 import ComingSoon from "./components/ComingSoon";
 
+import { NewsLetterContentData } from './assets/data/newsletters/1'
+
 function App() {
-
   return (
+    <div className="bg-black overflow-x-hidden" >
+
+      <Navbar />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/horizon" element={<Horizon />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/resources/finance-resources" element={<Finance />} />
+        <Route path="/resources/consulting-resources" element={<Consulting />} />
+        <Route path="/resources/product-resources" element={<ProdMan />} />
+
+        <Route path="/resources/newsletters" element={<Newsletters />} />
 
 
-      <div className="bg-black overflow-x-hidden" >
+        {/* Newsletters Routes */}
+        {NewsLetterContentData.map((data, index) => {
+          return(
+          <Route key={index} path={`/resources/newsletters/${data.id}`} element={<div> <NewsletterCard id={data.id} /> </div>} />
+        )})}
 
-        <Navbar />
-
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/horizon" element={<Horizon />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/resources/finance-resources" element={<Finance />} />
-          <Route path="/resources/consulting-resources" element={<Consulting />} />
-          <Route path="/resources/product-resources" element={<ProdMan />} />
-          
-          <Route path="/resources/newsletters" element={<Newsletters />} />
-
-          {/* Newsletters Routes */}
-          <Route path="/resources/newsletters/1" element={<News_1 />} />
-          <Route path="/resources/newsletters/2" element={<News_2 />} />
-          <Route path="/resources/newsletters/3" element={<News_3 />} />
-          <Route path="/resources/newsletters/4" element={<News_4 />} />
-          <Route path="/resources/newsletters/5" element={<News_5 />} />
-          <Route path="/resources/newsletters/6" element={<News_6 />} />
-          <Route path="/resources/newsletters/7" element={<News_7 />} />
-          <Route path="/resources/newsletters/8" element={<News_8 />} />
-          <Route path="/resources/newsletters/9" element={<News_9 />} />
-          <Route path="/resources/newsletters/10" element={<News_10 />} />
-
-          <Route path="/coming-soon" element={<ComingSoon />} />
+        <Route path="/coming-soon" element={<ComingSoon />} />
 
 
-        </Routes>
-        <Footer />
-      </div>
+      </Routes>
+      <Footer />
+    </div>
 
   )
 }
--- src/pages/resources-pages/Newsletters.jsx
+++ src/pages/resources-pages/Newsletters.jsx
@@ -6,7 +6,7 @@ const Newsletters = () => {
   return (
     <div className='min-h-screen w-full flex flex-col gap-10 my-10' >
       <h1 className='text-4xl mx-auto text-center' >Newsletters </h1>
-      <div className='w-full flex flex-col justify-evenly gap-10 ' >
+      <div className='w-full flex flex-col-reverse justify-evenly gap-10 ' >
         {/* Newsletters Card */}
         {
           NewsletterCardDisplayData.map((data, index) => {
