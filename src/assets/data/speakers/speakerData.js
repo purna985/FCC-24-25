fc270ad78d36b34229d078d30923acc388731710
Mahesh <mahesh.ruppa.1234@gmail.com>
2024-11-19 20:06:24 +0530

    Update speakerData.js

---
+++ src/assets/data/speakers/speakerData.js
@@ -0,0 +1,105 @@
+import ps_2024_1 from "../../pastspeakers/2024/Abhishek_Kar.jpg";
+import ps_2024_2 from "../../pastspeakers/2024/Arjun_Beri.jpg";
+import ps_2024_3 from "../../pastspeakers/2024/Ashwini_Bajaj.jpg";
+import ps_2024_4 from "../../pastspeakers/2024/Kadam_Aggarwal.jpg";
+import ps_2024_5 from "../../pastspeakers/2024/Mohit_Choudary.jpg";
+import ps_2024_6 from "../../pastspeakers/2024/Mrityunjay_Kumar.jpg";
+import ps_2024_7 from "../../pastspeakers/2024/Paresh_Gupta.jpg";
+import ps_2024_8 from "../../pastspeakers/2024/Pratik_Dhoot.jpg";
+import ps_2024_9 from "../../pastspeakers/2024/Raghavendra_Sai_Bhaskar_Tuta.jpg";
+import ps_2024_10 from "../../pastspeakers/2024/Subhankar_Murar.jpg";
+
+import ps_2023_1 from "../../pastspeakers/2023/Akhil_Sharma.png";
+import ps_2023_2 from "../../pastspeakers/2023/Gaurav_Mahindra.png";
+import ps_2023_3 from "../../pastspeakers/2023/Rishabh_Jain.png";
+import ps_2023_4 from "../../pastspeakers/2023/Subba_Perepa.png";
+
+export const SpeakersData_2024 = [
+  {
+    id: 1,
+    name: "Abhishek Kar",
+    image: ps_2024_1,
+    description: " ",
+  },
+  {
+    id: 2,
+    name: "Arjun Beri",
+    image: ps_2024_2,
+    description: " ",
+  },
+  {
+    id: 3,
+    name: "Ashwini Bajaj",
+    image: ps_2024_3,
+    description: " ",
+  },
+  {
+    id: 4,
+    name: "Kadam Aggarwal",
+    image: ps_2024_4,
+    description: " ",
+  },
+  {
+    id: 5,
+    name: "Mohit Choudary",
+    image: ps_2024_5,
+    description: " ",
+  },
+  {
+    id: 6,
+    name: "Mrityunjay Kumar",
+    image: ps_2024_6,
+    description: " ",
+  },
+  {
+    id: 7,
+    name: "Paresh Gupta",
+    image: ps_2024_7,
+    description: " ",
+  },
+  {
+    id: 8,
+    name: "Pratik_Dhoot",
+    image: ps_2024_8,
+    description: " ",
+  },
+  {
+    id: 9,
+    name: "Raghavendra Sai Bhaskar Tuta",
+    image: ps_2024_9,
+    description: " ",
+  },
+  {
+    id: 10,
+    name: "Subhankar Murar",
+    image: ps_2024_10,
+    description: " ",
+  },
+];
+
+export const SpeakersData_2023 = [
+  {
+    id: 1,
+    name: "Akhil Sharma",
+    image: ps_2023_1,
+    description: " ",
+  },
+  {
+    id: 2,
+    name: "Gaurav Mahindra",
+    image: ps_2023_2,
+    description: " ",
+  },
+  {
+    id: 3,
+    name: "Rishabh Jain",
+    image: ps_2023_3,
+    description: " ",
+  },
+  {
+    id: 4,
+    name: "Subba Perepa",
+    image: ps_2023_4,
+    description: " ",
+  },
+];
