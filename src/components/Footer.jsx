c35a77b224fa2f64c3b293e0991bfd1909035e0c
Mahesh <mahesh.ruppa.1234@gmail.com>
2024-12-27 23:59:09 +0530

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,9 +32,9 @@ function Footer() {
           </p>
         </div>
 
-        <div className=" mt-7 w-[80%] md:w-full flex justify-center md:justify-end gap-10 mx-auto md-4:flex-wrap">
+        <div className=" mt-7 w-[80%] md:w-full flex justify-center md:justify-end gap-10 mx-auto md-4:flex-col md-4:items-center">
           <div className="w-auto md:w-fit flex-col flex gap-3">
-            <ul className="flex flex-col gap-1 text-[1rem] mt-2 mx-6">
+            <ul className="flex flex-col gap-5 text-[1rem] mt-2 mx-6 md-4:flex-row">
               <li className="nav-item mb-2 hover:scale-110 transition-all duration-300  ">
                 <a href="https://www.instagram.com/fcc_iith?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
                   <FaInstagram className="w-8 h-8" />
@@ -57,60 +57,61 @@ function Footer() {
               </li>
             </ul>
           </div>
-          <div className="w-auto md:w-fit flex-col flex gap-3">
-            <h3 className="text-2xl font-semibold">About Us</h3>
-            <ul className="flex flex-col gap-1 text-[1rem]">
-              <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
-                <a href="/" className="font-medium text-md">
-                  FCC Leadership
-                </a>
-              </li>
-              <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
-                <a href="/" className="font-medium text-md" target="_blank">
-                  FCC Blogs
-                </a>
-              </li>
-              <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
-                <a href="/" className="font-medium text-md" target="_blank">
-                  Gallery
-                </a>
-              </li>
-              <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
-                <a href="/" className="font-medium text-md" target="_blank">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
+          <div className="flex md-4:flex-row gap-10">
+            <div className="w-auto md:w-fit flex-col flex gap-3">
+              <h3 className="text-2xl font-semibold">About Us</h3>
+              <ul className="flex flex-col gap-1 text-[1rem]">
+                <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                  <a href="/" className="font-medium text-md">
+                    FCC Leadership
+                  </a>
+                </li>
+                <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                  <a href="/" className="font-medium text-md" target="_blank">
+                    FCC Blogs
+                  </a>
+                </li>
+                <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                  <a href="/" className="font-medium text-md" target="_blank">
+                    Gallery
+                  </a>
+                </li>
+                <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                  <a href="/" className="font-medium text-md" target="_blank">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-          <div className="w-auto md:w-fit flex-col flex gap-3">
-            <h3 className="text-2xl font-semibold">Useful Links</h3>
-            <ul className="flex flex-col gap-1 text-[1rem]">
-              <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
-                <a href="/" className="font-medium text-md">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
-                <a
-                  href="/horizon"
-                  className="font-medium text-md"
-                  target="_blank"
-                >
-                  Horizon
-                </a>
-              </li>
-              <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
-                <a href="/" className="font-medium text-md" target="_blank">
-                  Sponsers
-                </a>
-              </li>
-              <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
-                <a href="/" className="font-medium text-md" target="_blank">
-                  Workshop
-                </a>
-              </li>
-              {/* <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+            <div className="w-auto md:w-fit flex-col flex gap-3">
+              <h3 className="text-2xl font-semibold">Useful Links</h3>
+              <ul className="flex flex-col gap-1 text-[1rem]">
+                <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                  <a href="/" className="font-medium text-md">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                  <a
+                    href="/horizon"
+                    className="font-medium text-md"
+                    target="_blank"
+                  >
+                    Horizon
+                  </a>
+                </li>
+                <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                  <a href="/" className="font-medium text-md" target="_blank">
+                    Sponsers
+                  </a>
+                </li>
+                <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                  <a href="/" className="font-medium text-md" target="_blank">
+                    Workshop
+                  </a>
+                </li>
+                {/* <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
                 <a
                   href="resources"
                   className="font-light text-md"
@@ -128,10 +129,11 @@ function Footer() {
                   Events
                 </a>
               </li> */}
-              {/* <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
+                {/* <li className="nav-item mb-2 hover:scale-105 transition-all duration-300  ">
                 <a href="/team" className="font-light text-md">Team</a>
               </li> */}
-            </ul>
+              </ul>
+            </div>
           </div>
 
           <div className="w-fit md:w-fit flex-col flex gap-3">
