930008c25c8494a42a163cb229c7ec13563811f1
root <root@DESKTOP-B6F7R7L.localdomain>
2025-06-16 06:22:30 +0000

    added colors to navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link, useLocation } from "react-router-dom";
 const Navbar = ({
   items,
   animationTime = 600,
-  particleCount = 15,
-  particleDistances = [90, 10],
+  particleCount = 20,
+  particleDistances = [120, 10],
   particleR = 100,
   timeVariance = 300,
   colors = [1, 2, 3, 1, 2, 3, 1, 4],
@@ -174,6 +174,11 @@ const Navbar = ({
       <style>
         {`
           :root {
+            --color-1: #2A2A72; 
+            --color-2: #6A05A1; 
+            --color-3: #9966CC; 
+            --color-4: #ffffff; 
+
             --linear-ease: linear(0, 0.068, 0.19 2.7%, 0.804 8.1%, 1.037, 1.199 13.2%, 1.245, 1.27 15.8%, 1.274, 1.272 17.4%, 1.249 19.1%, 0.996 28%, 0.949, 0.928 33.3%, 0.926, 0.933 36.8%, 1.001 45.6%, 1.013, 1.019 50.8%, 1.018 54.4%, 1 63.1%, 0.995 68%, 1.001 85%, 1);
           }
           .effect {
